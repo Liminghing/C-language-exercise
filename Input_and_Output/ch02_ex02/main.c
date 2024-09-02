f4faae3483cd7bbf7ba3ff9fd567d2a15e3d75b2
Liminghing <im349412@gmail.com>
2024-09-02 18:21:00 +0900

    Add Input and Output

---
+++ Input_and_Output/ch02_ex02/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  ch02_ex02
+//
+//  Created by 임준형 on 9/2/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("\"!@#$%%^&*()\"");
+    return 0;
+}
